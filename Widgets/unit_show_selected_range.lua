16456e8e5ae1774d6f8e2dd87528ee5019366a68
Helwor <friof@proton.me>
2026-02-03 14:55:14 +0100

    Implement ballistic (true) range !

--- Widgets/unit_show_selected_range.lua
+++ Widgets/unit_show_selected_range.lua
@@ -1,7 +1,12 @@
 -- mod version externalize the function to  draw units range WG.DrawUnitTypeRanges
+-- if not Game.modName:find('^Zero-K') then
+-- 	-- Echo(" is ", (VFS.LoadFile(LUAUI_DIRNAME .. 'Widgets/unit_show_selected_range.lua', VFS.ZIP) or ''):sub(1, 100))
+-- 	VFS.Include(LUAUI_DIRNAME .. 'Widgets/unit_show_selected_range.lua', widget, VFS.ZIP)
+-- 	return
+-- end
 function widget:GetInfo() return {
 	name    = "Show selected unit range",
-	author  = "very_bad_soldier / versus666",
+	author  = "very_bad_soldier / versus666, Helwor implement Ballistic Calc",
 	date    = "October 21, 2007 / September 08, 2010",
 	license = "GNU GPL v2",
 	layer   = 0,
@@ -17,40 +22,48 @@ local spGetUnitRulesParam   	= Spring.GetUnitRulesParam
 local spGetUnitWeaponState  	= Spring.GetUnitWeaponState
 local spIsGUIHidden 			= Spring.IsGUIHidden
 local spGetSelectedUnitsCount 	= Spring.GetSelectedUnitsCount
+local spGetUnitPosition			= Spring.GetUnitPosition
 
 local glColor            = gl.Color
 local glLineWidth        = gl.LineWidth
 local glDrawGroundCircle = gl.DrawGroundCircle
 
-
-local optMaxSelection = 20
+local CalcBallisticCircle 		= VFS.Include("LuaUI/Utilities/engine_range_circles.lua")
+local max_selection = 20
+local max_selection_ballistic = 1
+local use_ballistic = true
 local EMPTY_TABLE = {}
-local USE_BALLISTIC = true
 
-options_path = 'Settings/Interface/Defence and Cloak Ranges'
-options = {
-	showselectedunitrange = {
-		name = 'Show selected unit(s) range(s)',
-		type = 'bool',
-		value = false,
-		OnChange = function (self)
-			if self.value then
-				widgetHandler:UpdateCallIn("DrawWorldPreUnit")
-				widgetHandler:UpdateCallIn("CommandsChanged")
-				widget:CommandsChanged()
-			else
-				widgetHandler:RemoveCallIn("DrawWorldPreUnit")
-				widgetHandler:RemoveCallIn("CommandsChanged")
-			end
-		end,
-	},
-}
 local helk_path = 'Hel-K/' .. widget:GetInfo().name
-options.maxSelection = {
+options_path = 'Settings/Interface/Defence and Cloak Ranges'
+options = {}
+options_order = {
+	'showselectedunitrange',
+	'max_selection',
+	'use_ballistic',
+	'max_selection_ballistic',
+}
+options.showselectedunitrange = {
+	name = 'Show selected unit(s) range(s)',
+	type = 'bool',
+	value = false,
+	OnChange = function (self)
+		if self.value then
+			widgetHandler:UpdateCallIn("DrawWorldPreUnit")
+			widgetHandler:UpdateCallIn("CommandsChanged")
+			widget:CommandsChanged()
+		else
+			widgetHandler:RemoveCallIn("DrawWorldPreUnit")
+			widgetHandler:RemoveCallIn("CommandsChanged")
+		end
+	end,
+}
+options.max_selection = {
 	type = 'number',
 	name = 'Max Selection',
+	desc = 'Max selection under which we allow drawing of ranges',
 	min = 0, max = 301, step = 1,
-	value = optMaxSelection,
+	value = max_selection,
 	update_on_the_fly = true,
 	tooltipFunction = function(self)
 		if self.value == 301 then
@@ -61,9 +74,53 @@ options.maxSelection = {
 	end,
 	OnChange = function(self)
 		if self.value == 301 then
-			optMaxSelection = math.huge
+			max_selection = math.huge
 		else	
-			optMaxSelection = self.value
+			max_selection = self.value
+		end
+		if widget.CommandsChanged then
+			widget:CommandsChanged()
+		end
+	end,
+	path = helk_path,
+}
+
+options.use_ballistic = {
+	name = 'Use Ballistic Calc',
+	desc = 'Beware it might be heavy on big numbers',
+	type = 'bool',
+	value = use_ballistic,
+	OnChange = function (self)
+		use_ballistic = self.value
+		if widget.CommandsChanged then
+			widget:CommandsChanged()
+		end
+	end,
+	path = helk_path,
+}
+
+options.max_selection_ballistic = {
+	type = 'number',
+	name = 'Max Selection Ballistic',
+	desc = 'Max of units under which we calculate ballistic range.\n(if the option Use Ballistic Range is set)',
+	min = 0, max = 301, step = 1,
+	value = max_selection_ballistic,
+	update_on_the_fly = true,
+	tooltipFunction = function(self)
+		if self.value == 301 then
+			return 'unlimited'
+		else
+			return tostring(self.value)
+		end
+	end,
+	OnChange = function(self)
+		if self.value == 301 then
+			max_selection_ballistic = math.huge
+		else	
+			max_selection_ballistic = self.value
+		end
+		if widget.CommandsChanged then
+			widget:CommandsChanged()
 		end
 	end,
 	path = helk_path,
@@ -83,90 +140,95 @@ VFS.Include(LUAUI_DIRNAME ..'/Widgets/Include/weap_ranges.lua')
 
 local weapRanges = WG.weapRanges
 
-local function DrawRangeCircle(ux,uy,uz,range,r, color)
-	local strength = (1 - r/5)
+
+local function RangeColor(strengthIdx, color, ballistic)
+	local strength = (1 - strengthIdx/5)
 	if color then
 		glColor(color[1] * strength, color[2] * strength, color[3] - strength, color[4] or 0.35)
 	else
-		glColor(1.0 - (r / 5), 0, 0, 0.35)
+		glColor(1.0 - (strengthIdx / 5), ballistic and 1.0 or 0, 0, 0.35)
 	end
-	glDrawGroundCircle(ux, uy, uz, range, 40)
 end
+local function CircleVerts(verts)
+	for i = 1, #verts do
+		gl.Vertex(verts[i])
+	end
+	gl.Vertex(verts[1])
+end
+local function DrawRangeCircle(x, y, z, weap, range, rangeInfo, strengthIdx, color, use_ballistic)
+	RangeColor(strengthIdx, color, use_ballistic)
+	if use_ballistic then
+		local vertices = (WG.CalcBallisticCircle or CalcBallisticCircle)(x, y + rangeInfo['offY' .. weap], z, range, rangeInfo['weaponDef' .. weap])
+		gl.BeginEnd(GL.LINE_STRIP, CircleVerts, vertices)
+	else
+		glDrawGroundCircle(x, y, z, range, 40)
+	end
 
-
+end
 
 
 local knownUnits = setmetatable({},{__mode = 'v'})
 local reused = 0
 
-local function DrawComRanges(unitDefID, unitIDs, color, isEnemy)
-	for i = 1, #unitIDs do
-		local unitID = unitIDs[i]
-		local ux, uy, uz = spGetUnitViewPosition(unitID)
-		if ux then
+local function DrawComRanges(defID, units, color, isEnemy, use_ballistic)
+	for i, unitID in ipairs(units) do
+		local x,y,z = select(4, spGetUnitPosition(unitID, false , true))
+		if x then
 			local known = knownUnits[unitID]
-			if known then
-				weapRange1, weapRange2 = known[1], known[2]
-				reused = reused + 1
-				if weapRange1 then
-					DrawRangeCircle(ux,uy,uz,weapRange1,1)
-				end
-				if weapRange2 then
-					DrawRangeCircle(ux,uy,uz,weapRange2,2)
-				end
-			else
-				known = {}
+			if not known then
 				local weap1 = spGetUnitRulesParam(unitID, "comm_weapon_num_1")
-				if weap1 then
-					-- local weapRange = weapRanges[unitDefID][weap1] -- will be wrong is adv targetting module is bought
-					-- debugging
-					local weapRange =  spGetUnitWeaponState(unitID,weap1,"range")
-					-- if weapRange and weapRange2 and weapRange ~= weapRange2 then
-					-- 	Echo('Weapon State range is not static !',weapRange, weapRange2, math.round(os.clock()))
-					-- 	Spring.PlaySoundFile(LUAUI_DIRNAME .. 'Sounds/buildbar_add.wav', 0.95, 'ui')
-					-- end
-					
-					if weapRange then
-						DrawRangeCircle(ux,uy,uz,weapRange,1, color)
-						known[1] = weapRange
-					end
-				end
-
+				local range1 =  spGetUnitWeaponState(unitID, weap1, "range")
 				local weap2 = spGetUnitRulesParam(unitID, "comm_weapon_num_2")
-				if weap2 then
-					local weapRange = weapRanges[unitDefID][weap2]
-					-- local weapRange2 = spGetUnitWeaponState(unitID,weap2,"range")
-					if weapRange then
-						DrawRangeCircle(ux,uy,uz,weapRange,2, color)
-						known[2] = weapRange
-					end
-				end
+				local range2 = weap2 and spGetUnitWeaponState(unitID, weap2, "range")
+				local rangeInfo = weapRanges[defID]
+				known = {
+					rangeInfo,
+					weap1,
+					range1,
+					weap2,
+					range2,
+				}
 				knownUnits[unitID] = known
+			else
+				reused = reused + 1
 			end
-		end
-	end
-end
-local function DrawUnitsRanges(uDefID, uIDs, color)
-	local uWepRanges = weapRanges[uDefID]
-	if uWepRanges then
-		for i = 1, #uIDs do
-			local ux, uy, uz = spGetUnitViewPosition(uIDs[i])
-			if ux then
-				for r = 1, #uWepRanges do
-					DrawRangeCircle(ux,uy,uz,uWepRanges[r],r, color)
+				
+			local rangeInfo, weap1, range1, weap2, range2 = known[1], known[2], known[3], known[4], known[5]
+
+			if rangeInfo then
+				if weap1 then
+					DrawRangeCircle(x, y, z, weap1, range1, rangeInfo, 1, color, use_ballistic)
+				end
+				if weap2 then
+					DrawRangeCircle(ux,uy,uz, weap2, range2, rangeInfo, 1, color, use_ballistic)
 				end
 			end
 		end
 	end
 end
-local function DrawUnitTypeRanges(uDefID, uIDs, color, width, isEnemy)
+
+local function DrawUnitsRanges(defID, units, color, use_ballistic)
+	local rangeInfo = weapRanges[defID]
+	if rangeInfo then
+		for _, unitID in ipairs(units) do
+			-- local ux,uy,uz = select(4, spGetUnitPosition(unitID, false , true))
+			local x,y,z = spGetUnitPosition(unitID)
+			if x then
+				for weap, range in ipairs(rangeInfo) do
+					DrawRangeCircle(x, y, z, weap, range, rangeInfo, weap, color, use_ballistic)
+				end
+			end
+		end
+	end
+end
+local function DrawUnitTypeRanges(defID, units, use_ballistic, color, width, isEnemy)
 	if width then
 		glLineWidth(width)
 	end
-	if commDefIDs[uDefID] then -- Dynamic comm have different ranges and different weapons activated
-		DrawComRanges(uDefID, uIDs, color, isEnemy)
+	if commDefIDs[defID] then -- Dynamic comm have different ranges and different weapons activated
+		DrawComRanges(defID, units, color, isEnemy, use_ballistic)
 	else
-		DrawUnitsRanges(uDefID, uIDs, color)
+		DrawUnitsRanges(defID, units, color, use_ballistic)
 	end
 	if width then
 		glLineWidth(1)
@@ -183,11 +245,12 @@ end
 -- end
 
 
-local selUnits
+local selUnits, selCount
 
 function widget:CommandsChanged()
-	if spGetSelectedUnitsCount() <= optMaxSelection then
-		selUnits = spGetSelUnitsSorted()
+	selCount = spGetSelectedUnitsCount()
+	if selCount <= max_selection then
+		selUnits = WG.selectionDefID or spGetSelUnitsSorted()
 	else
 		selUnits = EMPTY_TABLE
 	end
@@ -200,8 +263,8 @@ function widget:DrawWorldPreUnit()
 
 	glLineWidth(1.5)
 
-	for uDefID, uIDs in pairs(selUnits) do
-		DrawUnitTypeRanges(uDefID, uIDs)
+	for defID, units in pairs(selUnits) do
+		DrawUnitTypeRanges(defID, units, use_ballistic and selCount <= max_selection_ballistic)
 	end
 
 	glColor(1, 1, 1, 1)
@@ -216,4 +279,8 @@ end
 
 function widget:Shutdown()
 	WG.DrawUnitTypeRanges = nil
+end
+
+if f then
+	f.DebugWidget(widget)
 end
