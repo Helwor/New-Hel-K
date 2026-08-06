441833adcf43637fc4892c92a5199cfd9fbc784c
Helwor <friof@proton.me>
2026-08-06 07:46:34 +0100

    Adapt to latest version (icon scaling)

--- Widgets/gui_chili_selections_and_cursortip.lua
+++ Widgets/gui_chili_selections_and_cursortip.lua
@@ -57,7 +57,7 @@ local yellow = '\255\255\255\1'
 
 local selectionTooltip
 local singleSelectionTooltip
-
+local selectionChanged = false
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
@@ -291,6 +291,19 @@ local ctrlFilterUnitIncluded = false
 --------------------------------------------------------------------------------
 -- Settings
 
+local function ResetWindows()
+	if selectionWindow then
+		selectionWindow.Reinitialise()
+	end
+	if tooltipWindow and tooltipWindow.Reinitialise then
+		tooltipWindow.Reinitialise()
+	end
+	--tooltipWindow = (WG.Modding_TooltipOverride and WG.Modding_TooltipOverride()) or GetTooltipWindow()
+	if selectedUnitsList[1] then
+		widget:SelectionChanged()
+		widget:CommandsChanged()
+	end
+end
 options_path = 'Settings/HUD Panels/Tooltip'
 local selPath = 'Settings/HUD Panels/Selected Units Panel'
 
@@ -298,12 +311,12 @@ options_order = {
 	--tooltip
 	'tooltip_delay', 'independant_world_tooltip_delay',
 	'show_for_units', 'show_for_wreckage', 'show_for_unreclaimable', 'showdrawtooltip','showterratooltip',
-	'showDrawTools', 'tooltip_opacity',
+	'showDrawTools', 'tooltipScale', 'tooltip_opacity',
 	
 	--selected units
 	'selection_opacity', 'allowclickthrough', 'tooltipThroughPanels', 'groupbehaviour', 'showgroupinfo', 'sortByHealth',
 
-	'uniticon_size', 'manualWeaponReloadBar', 'jumpReloadBar',
+	'uniticon_size', 'matchIconSizes', 'manualWeaponReloadBar', 'jumpReloadBar', 'selectionScale',
 	'fancySkinning', 'leftPadding',
 }
 
@@ -371,6 +384,12 @@ options = {
 	tooltipThroughPanels = {
 		name='Allow hovering through', type='bool', value=false,
 		desc = 'Show tooltips for units behind empty interface panels.',
+	},
+	tooltipScale = {
+		name = "UI Scale",
+		type = "number",
+		value = 1, min = 1, max = 3, step = 0.01,
+		OnChange = ResetWindows,
 	},
 	tooltip_opacity = {
 		name = "Opacity",
@@ -443,14 +462,23 @@ options = {
 		name = 'Icon size on selection list',
 		--desc = 'Determines how small the icon in selection list need to be.',
 		type = 'number',
-		min=30,max=100,step=1,
+		min=30,max=160,step=1,
 		value = 57,
 		path = selPath,
 		OnChange = function(self)
 			if selectionWindow then
 				selectionWindow.SetSelectionIconSize(self.value)
 			end
+			ResetWindows()
 		end,
+	},
+	matchIconSizes = {
+		name="Match Icon Sizes",
+		type='bool',
+		value= true,
+		desc = "Match single selection and tooltip icon size to multi-selection icon size.",
+		path = selPath,
+		OnChange = ResetWindows
 	},
 	manualWeaponReloadBar = {
 		name="Show Unit's Special Weapon Status",
@@ -473,6 +501,13 @@ options = {
 		OnChange = function(self)
 			showJumpReload = self.value
 		end,
+	},
+	selectionScale = {
+		name = "UI Scale",
+		type = "number",
+		value = 1, min = 1, max = 3, step = 0.01,
+		OnChange = ResetWindows,
+		path = selPath,
 	},
 	fancySkinning = {
 		name = 'Fancy Skinning',
@@ -1207,30 +1242,33 @@ end
 --------------------------------------------------------------------------------
 -- Unit tooltip window components
 
-local function GetBarWithImage(parentControl, name, initY, imageFile, color, colorFunc)
+local function GetBarWithImage(parentControl, name, initY, imageFile, scale, color, colorFunc)
+	local iconSize = math.floor(ICON_SIZE * scale + 0.5)
+	local barSize = math.floor(BAR_SIZE * scale + 0.5)
+	local barFont = math.floor(BAR_FONT * scale + 0.5)
 	local image = Chili.Image:New{
 		name = name .. "_image",
 		x = 2,
 		y = initY,
-		width = ICON_SIZE,
-		height = ICON_SIZE,
+		width = iconSize,
+		height = iconSize,
 		file = imageFile,
 		parent = parentControl,
 	}
 	
 	local bar = Chili.Progressbar:New {
 		name = name .. "_bar",
-		x = ICON_SIZE + 3,
+		x = iconSize + 3,
 		y = initY,
 		right = 0,
-		height = BAR_SIZE,
+		height = barSize,
 		max = 1,
 		color = color,
 		itemMargin  = {0,0,0,0},
 		itemPadding = {0,0,0,0},
 		padding     = {0,0,0,0},
 		caption = '',
-		objectOverrideFont = WG.GetFont(BAR_FONT),
+		objectOverrideFont = WG.GetFont(barFont),
 		parent = parentControl
 	}
 	
@@ -1489,6 +1527,11 @@ end
 --------------------------------------------------------------------------------
 -- Group buttons window
 
+local function GetMultiIconScale()
+	local width = options.uniticon_size.value - 2 -- Parent padding
+	local height = math.floor(options.uniticon_size.value * 0.79 + 0.5)
+	return width, height
+end
 local function GetUnitGroupIconButton(parentControl)
 	
 	local unitDefID
@@ -1522,12 +1565,13 @@ local function GetUnitGroupIconButton(parentControl)
 		parent = holder
 	}
 	
+	local imageWidth, imageHeight = GetMultiIconScale()
 	local unitImage = Chili.Image:New{
 		keepAspect = false,
 		x = 0,
 		y = 0,
-		right = 0,
-		bottom = "20%",
+		width = imageWidth,
+		height = imageHeight,
 		padding = {0,0,0,0},
 		parent = holder,
 		OnClick = {
@@ -1616,7 +1660,7 @@ local function GetUnitGroupIconButton(parentControl)
 		end
 		
 		unitImage.tooltip = GetUnitSelectionTooltip(ud, unitDefID, unitID)
-		unitImage.file = "#" .. unitDefID
+		unitImage.file = WG.GetRectangleBuildTexture(ud)
 		unitImage.file2 = GetUnitBorder(unitDefID)
 		unitImage:Invalidate()
 	end
@@ -2030,23 +2074,37 @@ end
 
 local function GetSingleUnitInfoPanel(parentControl, isTooltipVersion)
 	local selectedUnitID
+	local scale = isTooltipVersion and options.tooltipScale.value or options.selectionScale.value
+	local leftWidth = math.floor((LEFT_WIDTH - 2) * scale + 2 + 0.5)
+	local rightWidth = math.floor(RIGHT_WIDTH * scale + 0.5)
+	local picHeight = math.floor(PIC_HEIGHT * scale + 0.5)
+	local fontSize = math.floor(IMAGE_FONT * scale + 0.5)
+	local nameFont = math.floor(NAME_FONT * scale + 0.5)
+	local descFont = math.floor(DESC_FONT * scale + 0.5)
+	local iconSize = math.floor(ICON_SIZE * scale + 0.5)
+	local barSpacing = math.floor(BAR_SPACING * scale + 0.5)
+	local leftSpacing = math.floor(LEFT_SPACE * scale + 0.5)
+	
+	if options.matchIconSizes.value then
+		leftWidth, picHeight = GetMultiIconScale()
+	end
 	
 	local leftPanel = Chili.Control:New{
 		name = "leftPanel",
 		x = 0,
 		y = 0,
-		width = LEFT_WIDTH,
-		minWidth = LEFT_WIDTH,
+		width = leftWidth,
+		minWidth = leftWidth,
 		autosize = true,
 		padding = {0,2,0,2},
 		parent = parentControl,
 	}
 	local rightPanel = Chili.Control:New{
 		name = "rightPanel",
-		x = LEFT_WIDTH,
+		x = leftWidth,
 		y = 0,
-		width = RIGHT_WIDTH,
-		minWidth = RIGHT_WIDTH,
+		width = rightWidth,
+		minWidth = rightWidth,
 		autosize = true,
 		padding = {2,2,0,2},
 		parent = parentControl,
@@ -2057,8 +2115,8 @@ local function GetSingleUnitInfoPanel(parentControl, isTooltipVersion)
 		name = "unitImage",
 		x = 0,
 		y = 0,
-		right = 0,
-		height = PIC_HEIGHT,
+		width = leftWidth,
+		height = picHeight,
 		keepAspect = false,
 		file = imageFile,
 		parent = leftPanel,
@@ -2082,26 +2140,26 @@ local function GetSingleUnitInfoPanel(parentControl, isTooltipVersion)
 		end
 	end
 	
-	local unitNameUpdate = GetImageWithText(rightPanel, "unitNameUpdate", 1, nil, nil, NAME_FONT, nil, 2, 1)
+	local unitNameUpdate = GetImageWithText(rightPanel, "unitNameUpdate", 1, nil, nil, nameFont, nil, 2, 1)
 	
 	local unitDesc = Chili.TextBox:New{
 		name = "unitDesc",
 		x = 4,
-		y = 25,
+		y = math.floor(25 * scale + 0.5),
 		right = 0,
-		height = BAR_SIZE,
-		objectOverrideFont = WG.GetFont(DESC_FONT),
+		height = math.floor(BAR_SIZE * scale + 0.5),
+		objectOverrideFont = WG.GetFont(descFont),
 		parent = rightPanel,
 	}
 	
-	local costInfoUpdate = GetImageWithText(leftPanel, "costInfoUpdate", PIC_HEIGHT + 4, IMAGE.COST, nil, nil, ICON_SIZE, 4)
-	local metalInfoUpdate = GetImageWithText(leftPanel, "metalInfoUpdate", PIC_HEIGHT + LEFT_SPACE + 4, IMAGE.METAL, nil, nil, ICON_SIZE, 4)
-	local energyInfoUpdate = GetImageWithText(leftPanel, "energyInfoUpdate", PIC_HEIGHT + 2*LEFT_SPACE + 4, IMAGE.ENERGY, nil, nil, ICON_SIZE, 4)
-	local maxHealthLabel = GetImageWithText(rightPanel, "maxHealthLabel", PIC_HEIGHT + 4, IMAGE.HEALTH, nil, NAME_FONT, ICON_SIZE, 2, 2)
+	local costInfoUpdate = GetImageWithText(leftPanel, "costInfoUpdate", picHeight + 4, IMAGE.COST, nil, fontSize, iconSize, 4)
+	local metalInfoUpdate = GetImageWithText(leftPanel, "metalInfoUpdate", picHeight + leftSpacing + 4, IMAGE.METAL, nil, fontSize, iconSize, 4)
+	local energyInfoUpdate = GetImageWithText(leftPanel, "energyInfoUpdate", picHeight + 2*leftSpacing + 4, IMAGE.ENERGY, nil, fontSize, iconSize, 4)
+	local maxHealthLabel = GetImageWithText(rightPanel, "maxHealthLabel", picHeight + 4, IMAGE.HEALTH, nil, nameFont, iconSize, 2, 2)
 	
-	local minWindLabel = GetImageWithText(leftPanel, "minWindLabel", PIC_HEIGHT + LEFT_SPACE + 4, IMAGE.WIND_SPEED, nil, nil, ICON_SIZE, 4)
-	local healthBarUpdate = GetBarWithImage(rightPanel, "healthBarUpdate", PIC_HEIGHT + 4, IMAGE.HEALTH, {0, 1, 0, 1}, GetHealthColor)
-	local unitpicBadgeUpdate = GetImage(unitImage, "costInfoUpdate", 4, IMAGE.NO_AMMO, ICON_SIZE, 4)
+	local minWindLabel = GetImageWithText(leftPanel, "minWindLabel", picHeight + leftSpacing + 4, IMAGE.WIND_SPEED, nil, fontSize, iconSize, 4)
+	local healthBarUpdate = GetBarWithImage(rightPanel, "healthBarUpdate", picHeight + 4, IMAGE.HEALTH, scale, {0, 1, 0, 1}, GetHealthColor)
+	local unitpicBadgeUpdate = GetImage(unitImage, "costInfoUpdate", 4, IMAGE.NO_AMMO, iconSize, 4)
 	
 	local metalInfo
 	local energyInfo
@@ -2111,26 +2169,26 @@ local function GetSingleUnitInfoPanel(parentControl, isTooltipVersion)
 		playerNameLabel = Chili.Label:New{
 			name = "playerNameLabel",
 			x = 4,
-			y = PIC_HEIGHT + 31,
+			y = picHeight + 31,
 			right = 0,
 			height = BAR_FONT,
-			objectOverrideFont = WG.GetFont(IMAGE_FONT),
+			objectOverrideFont = WG.GetFont(fontSize),
 			parent = rightPanel,
 		}
 		spaceClickLabel = Chili.Label:New{
 			name = "spaceClickLabel",
 			x = 4,
-			y = PIC_HEIGHT + 55,
+			y = picHeight + 55,
 			right = 0,
 			height = 18,
-			objectOverrideFont = WG.GetFont(IMAGE_FONT),
+			objectOverrideFont = WG.GetFont(fontSize),
 			caption = green .. WG.Translate("interface", "space_click_show_stats"),
 			parent = rightPanel,
 		}
-		costInfoPanel = GetCostInfoPanel(rightPanel, PIC_HEIGHT + 4)
+		costInfoPanel = GetCostInfoPanel(rightPanel, picHeight + 4)
 	else
-		shieldBarUpdate = GetBarWithImage(rightPanel, "shieldBarUpdate", PIC_HEIGHT + 4, IMAGE.SHIELD, {0.3,0,0.9,1})
-		buildBarUpdate = GetBarWithImage(rightPanel, "buildBarUpdate", PIC_HEIGHT + 58, IMAGE.BUILD, {0.8,0.8,0.2,1})
+		shieldBarUpdate = GetBarWithImage(rightPanel, "shieldBarUpdate", picHeight + 4, IMAGE.SHIELD, scale, {0.3,0,0.9,1})
+		buildBarUpdate = GetBarWithImage(rightPanel, "buildBarUpdate", picHeight + 58, IMAGE.BUILD, scale, {0.8,0.8,0.2,1})
 	end
 
 	local prevUnitID, prevUnitDefID, prevFeatureID, prevFeatureDefID, prevVisible, prevMorphTime, prevMorphCost, prevMousePlace
@@ -2155,8 +2213,8 @@ local function GetSingleUnitInfoPanel(parentControl, isTooltipVersion)
 		local mm, mu, em, eu = GetUnitResources(unitID)
 		local showMetalInfo = false
 		if mm then
-			metalInfoUpdate(true, FormatPlusMinus(mm - mu), IMAGE.METAL, PIC_HEIGHT + LEFT_SPACE + 4)
-			energyInfoUpdate(true, FormatPlusMinus(em - eu), IMAGE.ENERGY, PIC_HEIGHT + 2*LEFT_SPACE + 4)
+			metalInfoUpdate(true, FormatPlusMinus(mm - mu), IMAGE.METAL, picHeight + LEFT_SPACE + 4)
+			energyInfoUpdate(true, FormatPlusMinus(em - eu), IMAGE.ENERGY, picHeight + 2*LEFT_SPACE + 4)
 			showMetalInfo = true
 		else
 			metalInfoUpdate(false)
@@ -2172,10 +2230,10 @@ local function GetSingleUnitInfoPanel(parentControl, isTooltipVersion)
 				if shieldCurrentPower and shieldPower then
 					shieldBarUpdate(true, nil, shieldCurrentPower, shieldPower, (shieldCurrentPower < shieldPower) and GetUnitShieldRegenString(unitID, ud))
 				end
-				healthPos = PIC_HEIGHT + 4 + BAR_SPACING
+				healthPos = picHeight + 4 + barSpacing
 			else
 				shieldBarUpdate(false)
-				healthPos = PIC_HEIGHT + 4
+				healthPos = picHeight + 4
 			end
 		end
 		
@@ -2190,7 +2248,7 @@ local function GetSingleUnitInfoPanel(parentControl, isTooltipVersion)
 				local metalMake, metalUse, energyMake,energyUse = Spring.GetUnitResources(unitID)
 				
 				local currentBuild = GetCurrentBuildSpeed(unitID, unhandicappedSpeed)
-				buildBarUpdate(true, (healthPos or (PIC_HEIGHT + 4)) + BAR_SPACING, currentBuild or 0, buildSpeed)
+				buildBarUpdate(true, (healthPos or (picHeight + 4)) + barSpacing, currentBuild or 0, buildSpeed)
 			else
 				buildBarUpdate(false)
 			end
@@ -2210,7 +2268,7 @@ local function GetSingleUnitInfoPanel(parentControl, isTooltipVersion)
 		local metal, _, energy, _, _ = Spring.GetFeatureResources(featureID)
 		local leftOffset = -2
 		if unitDefID then
-			leftOffset = PIC_HEIGHT + LEFT_SPACE
+			leftOffset = picHeight + LEFT_SPACE
 		end
 		metalInfoUpdate(true, Format(metal), IMAGE.METAL_RECLAIM, leftOffset + 4)
 		energyInfoUpdate(true, Format(energy), IMAGE.ENERGY_RECLAIM, leftOffset + LEFT_SPACE + 4)
@@ -2303,15 +2361,15 @@ local function GetSingleUnitInfoPanel(parentControl, isTooltipVersion)
 			if featureUnitDefID then
 				unitDefID = featureUnitDefID
 				if playerNameLabel then
-					playerNameLabel:SetPos(nil, PIC_HEIGHT + 8, nil, nil, nil, true)
-					spaceClickLabel:SetPos(nil, PIC_HEIGHT + 32, nil, nil, nil, true)
+					playerNameLabel:SetPos(nil, picHeight + 8, nil, nil, nil, true)
+					spaceClickLabel:SetPos(nil, picHeight + 32, nil, nil, nil, true)
 				end
 			else
 				costInfoUpdate(false)
 				unitNameUpdate(true, featureTooltip, nil)
 				if playerNameLabel then
-					playerNameLabel:SetPos(nil, PIC_HEIGHT - 13, nil, nil, nil, true)
-					spaceClickLabel:SetPos(nil, PIC_HEIGHT + 12, nil, nil, nil, true)
+					playerNameLabel:SetPos(nil, picHeight - 13, nil, nil, nil, true)
+					spaceClickLabel:SetPos(nil, picHeight + 12, nil, nil, nil, true)
 				end
 			end
 			
@@ -2326,7 +2384,7 @@ local function GetSingleUnitInfoPanel(parentControl, isTooltipVersion)
 				unitImage.tooltip = GetSingleUnitSelectionTooltip(ud, unitDefID)
 			end
 
-			unitImage.file = "#" .. unitDefID
+			unitImage.file = WG.GetRectangleBuildTexture(ud)
 			unitImage.file2 = GetUnitBorder(unitDefID)
 			unitImage:Invalidate()
 
@@ -2339,7 +2397,7 @@ local function GetSingleUnitInfoPanel(parentControl, isTooltipVersion)
 			elseif smallCostDisplay >= 10000 then
 				smallCostDisplay = math.floor(smallCostDisplay / 1000) .. "k"
 			end
-			costInfoUpdate(true, cyan .. smallCostDisplay, IMAGE.COST, PIC_HEIGHT + 4)
+			costInfoUpdate(true, cyan .. smallCostDisplay, IMAGE.COST, picHeight + 4)
 			
 			local extraTooltip, healthOverride
 			if not (unitID or featureID) then
@@ -2357,29 +2415,29 @@ local function GetSingleUnitInfoPanel(parentControl, isTooltipVersion)
 			
 			if unitID then
 				if playerNameLabel then
-					playerNameLabel:SetPos(nil, PIC_HEIGHT + 32, nil, nil, nil, true)
-					spaceClickLabel:SetPos(nil, PIC_HEIGHT + 56, nil, nil, nil, true)
+					playerNameLabel:SetPos(nil, picHeight + 32, nil, nil, nil, true)
+					spaceClickLabel:SetPos(nil, picHeight + 56, nil, nil, nil, true)
 				end
 			end
 			if (not (unitID and visible)) and not featureDefID then
 				healthBarUpdate(false)
-				local maxHealthPos = PIC_HEIGHT + 4
+				local maxHealthPos = picHeight + 4
 				if blueprint and costInfoPanel then
 					costInfoPanel(true, false, unitCost, "Cost:", nil, 46)
 					UpdateBuildTime(unitDefID, unitCost)
 					maxHealthPos = maxHealthPos + LEFT_SPACE
 					if spaceClickLabel then
-						spaceClickLabel:SetPos(nil, PIC_HEIGHT + LEFT_SPACE + 30, nil, nil, nil, true)
+						spaceClickLabel:SetPos(nil, picHeight + LEFT_SPACE + 30, nil, nil, nil, true)
 					end
 				elseif morphTime and costInfoPanel then
 					costInfoPanel(true, SecondsToMinutesSeconds(morphTime), morphCost, "Morph:", nil, 58)
 					morphShown = true
 					maxHealthPos = maxHealthPos + LEFT_SPACE
 					if spaceClickLabel then
-						spaceClickLabel:SetPos(nil, PIC_HEIGHT + LEFT_SPACE + 30, nil, nil, nil, true)
+						spaceClickLabel:SetPos(nil, picHeight + LEFT_SPACE + 30, nil, nil, nil, true)
 					end
 				elseif spaceClickLabel and not unitID then
-					spaceClickLabel:SetPos(nil, PIC_HEIGHT + 34, nil, nil, nil, true)
+					spaceClickLabel:SetPos(nil, picHeight + 34, nil, nil, nil, true)
 				end
 				maxHealthShown = true
 				local health = Spring.Utilities.GetUnitMaxHealth and Spring.Utilities.GetUnitMaxHealth(unitID, unitDefID, healthOverride) or healthOverride or ud.health
@@ -2476,6 +2534,7 @@ end
 -- Tooltip window handler
 
 local function GetTooltipWindow()
+	local rightWidth = math.floor(RIGHT_WIDTH * options.tooltipScale.value + 0.5)
 	local tempOpacity = false
 	local window = Chili.Window:New{
 		name = "tooltipWindow",
@@ -2485,7 +2544,7 @@ local function GetTooltipWindow()
 		resizable = false,
 		draggable = false,
 		autosize  = true,
-		minWidth = RIGHT_WIDTH,
+		minWidth = rightWidth,
 		noFont = true,
 		padding = {6,4,6,2},
 		color = {1, 1, 1, options.tooltip_opacity.value},
@@ -2497,7 +2556,7 @@ local function GetTooltipWindow()
 		name = "textTooltip",
 		x = 0,
 		y = 4,
-		width = RIGHT_WIDTH - 10,
+		width = rightWidth - 10,
 		height = 5,
 		valign = "ascender",
 		autoHeight = true,
@@ -2512,23 +2571,31 @@ local function GetTooltipWindow()
 		window = window
 	}
 	
-
+	function externalFunctions.Reinitialise()
+		while window.children[1] do
+			window.children[1]:Dispose()
+		end
+		textTooltip = Chili.TextBox:New{
+			name = "textTooltip",
+			x = 0,
+			y = 4,
+			width = rightWidth - 10,
+			height = 5,
+			valign = "ascender",
+			autoHeight = true,
+			objectOverrideFont = WG.GetFont(TOOLTIP_FONT),
+			parent = window,
+		}
+		textTooltip:SetVisibility(false)
+		unitDisplay = GetSingleUnitInfoPanel(window, true)
+	end
+	
 	function externalFunctions.LanguageChange()
 		unitDisplay.LanguageChange()
 	end
 
 	function externalFunctions.SetVisible(newVisible)
 		window:SetVisibility(newVisible)
-	end
-
-	function externalFunctions.SetDefaultWidth()
-		window.minWidth = RIGHT_WIDTH
-		window:Resize(RIGHT_WIDTH - 10)
-	end
-
-	function externalFunctions.SetWidth(w)
-		window.minWidth = w
-		window:Resize(w - 10)
 	end
 
 	function externalFunctions.SetOpacity(opacity)
@@ -2883,6 +2950,16 @@ local function GetSelectionWindow()
 	
 	local externalFunctions = {}
 	
+	function externalFunctions.Reinitialise()
+		while mainPanel.children[1] do
+			mainPanel.children[1]:Dispose()
+		end
+		singleUnitDisplay = GetSingleUnitInfoPanel(mainPanel, false)
+		multiUnitDisplay = GetMultiUnitInfoPanel(mainPanel)
+		selectionStatsDisplay = GetSelectionStatsDisplay(mainPanel)
+		singleUnitDisplay.SetVisible(false)
+	end
+	
 	function externalFunctions.LanguageChange()
 		singleUnitDisplay.LanguageChange()
 		multiUnitDisplay.LanguageChange()
@@ -2935,6 +3012,8 @@ local function GetSelectionWindow()
 	
 	function externalFunctions.SetSkin(className)
 		SetPanelSkin(mainPanel, className)
+		mainPanel.padding[2] = 14
+		mainPanel:Invalidate()
 	end
 	
 	function externalFunctions.SetLeftPadding(padding)
@@ -3064,7 +3143,15 @@ function widget:KeyRelease(key, modifier, isRepeat)
 	end
 end
 
+function widget:SelectionChanged()
+	selectionChanged = true
+end
+
 function widget:CommandsChanged() -- like SelectionChanged, but only called once after all the filtering is done and the selection is finalized
+	if not selectionChanged then
+		return
+	end
+	selectionChanged = false
 	local selectionDefIDs = spGetSelectedUnitsSorted() -- able to tell the defID of units selected in the fog (e.g. via godmode or specfullview 2)
 	local defIDs = {}
 	local newSelection = {defIDs = defIDs}
@@ -3142,6 +3229,8 @@ function widget:Initialize()
 
 	LanguageUpdate()
 	WG.InitializeTranslation(LanguageUpdate, "gui_chili_selections_and_cursortip.lua")
+	widget:SelectionChanged()
+	widget:CommandsChanged()
 end
 
 function widget:UnitDestroyed(unitID)
