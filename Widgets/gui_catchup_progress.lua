154bc0eb33c3f0a48fd46610ab167d9fefc283ab
Helwor <friof@proton.me>
2026-09-05 05:46:46 +0200

    Implement graphical catching speed display !

--- Widgets/gui_catchup_progress.lua
+++ Widgets/gui_catchup_progress.lua
@@ -10,15 +10,34 @@ function widget:GetInfo()
 	handler   = true,
 	}
 end
+ -- FIXME can't find how to display some special characters with this
+-- local fhDraw = fontHandler.Draw
+-- local font = "LuaUI/Fonts/FreeSansBold_14"
+-- local runFont = "LuaUI/Fonts/FreeMonoBold_12"
+-- local runFont = "LuaUI/Fonts/FreeSansBold_14" 
+-- local UseFont = fontHandler.UseFont
+------
+local triangle = string.char(226, 150, 186) -- ►
 
+local timeCounter = 0
+local gameTimePerSecond = 1
+local lastGameTime = 0
+local offy = -75
+local runString = false
+local runFontSize = 12
+local runWidth, runHeight
+local maxRunLength = 10
+local baseRunWidth
 local height = 5
 local currentFrame = Spring.GetGameFrame()
 local gameProg = currentFrame
 local gl = gl
+local Spring = Spring
+local math = math
 local vsx, vsy
 local width
 
-function widget:GameProgress(f)
+function widget:GameProgress(f) -- knowing we're catching up via GameProgress is not ideal
 	gameProg = f
 end
 
@@ -51,8 +70,39 @@ local function TimeFormat(t)
 	end
 	return h..m..sec
 end
+
+function widget:Update(dt)
+	timeCounter = timeCounter + dt
+	if timeCounter >= 1 then
+		local gameTime = Spring.GetGameSeconds()
+		local gameTimePassed = gameTime - lastGameTime
+		lastGameTime = gameTime
+		gameTimePerSecond = gameTimePassed / timeCounter
+		Echo("gameTimePerSecond is ", gameTimePerSecond)
+		if gameTimePerSecond > 1.1 then
+			local int = math.min(maxRunLength, math.floor(gameTimePerSecond + 0.5))
+			runString = triangle:rep(int) 
+			runWidth = baseRunWidth * int
+		else
+			runString = false
+		end
+		timeCounter = 0
+	end
+end
+
 function widget:DrawScreen()
-	if gameProg - currentFrame > 100 then
+	if runString then
+		-- draw speed run triangles
+		gl.Color(0,0.8,0,1)
+		-- FIXME can't find how to display special character this way
+		-- UseFont(runFont)
+		-- fhDraw(runString, math.floor(vsx - runWidth - 2 + 0.5), math.floor(vsy + offy + 0.5))
+		------
+		gl.Text(runString, math.floor(vsx - runWidth - 2 + 0.5), math.floor(vsy + offy + 0.5), runFontSize, '')
+	end
+	if gameProg - currentFrame > 30 then
+		gl.Color(1,1,1,1)
+		-- draw bar
 		gl.LineWidth(1)
 		gl.PushMatrix()
 		gl.Translate(vsx/2, vsy-height/2, 0)
@@ -61,11 +111,11 @@ function widget:DrawScreen()
 		gl.Color(0.8,0.5,0,1)
 		gl.Shape(GL.LINE_STRIP, frame)
 		gl.LineWidth(1)
-		-- gl.Text(TimeFormat(currentFrame)..'/'..TimeFormat(gameProg), (vsx + width)/2, vsy-height/2, 10, 'vno')
 		gl.PopMatrix()
 		gl.Text(TimeFormat(gameProg), vsx/2, vsy-height/2, 10, 'cvno')
-		gl.Color(1,1,1,1)
+		-- draw run triangles
 	end
+	gl.Color(1,1,1,1)
 end
 
 function widget:GetViewSizes(x, y)
@@ -73,6 +123,13 @@ function widget:GetViewSizes(x, y)
 	width = vsx
 end
 
+
 function widget:Initialize()
 	widget:GetViewSizes(Spring.GetViewSizes())
+    local testFont = WG.Chili.Font:New({name = font, size = runFontSize})
+    local testString= ("%s"):format(triangle)
+    baseRunWidth, runHeight = testFont:GetTextWidth(testString), testFont:GetTextHeight(testString)
+    testFont:Dispose()
+    testFont = nil
 end
+
